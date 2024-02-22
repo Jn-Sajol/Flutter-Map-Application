c1fe14657ba4d5d5c3e9b6f5ce3eeebb228f2ecb
Jn-Sajol <julkar10121@gmail.com>
2024-02-22 18:44:44 +0600

    adding profile page

--- lib/pages/profile_page.dart
+++ lib/pages/profile_page.dart
@@ -1,26 +1,96 @@
+import 'dart:js_util';
+
 import 'package:flutter/material.dart';
 
 class ProfilePage extends StatelessWidget {
-   ProfilePage({super.key});
-final  List<String> menu = ['add','edit'];
+  ProfilePage({super.key});
+  final List<String> menu = ['add', 'edit'];
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
-    appBar: AppBar(
-    title: const Text('Profile page'),
-    actions: [
-    PopupMenuButton(
-    itemBuilder:(context) => [
-    const PopupMenuItem(
-    child: Text('Edit')),
-    const PopupMenuItem(
-    child: Text('add')),
-    const PopupMenuItem(
-    child: Text('setting'))
-    ]
-    ),
-    ],
-    ),
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Profile page'),
+        actions: [
+          PopupMenuButton(
+              itemBuilder: (context) => [
+                    const PopupMenuItem(child: Text('Edit')),
+                    const PopupMenuItem(child: Text('add')),
+                    const PopupMenuItem(child: Text('setting'))
+                  ]),
+        ],
+      ),
+      body: const Padding(
+        padding: EdgeInsets.all(12.0),
+        child: Column(
+          children: [
+            SizedBox(
+              height: 14,
+            ),
+            ClipRRect(
+            borderRadius: BorderRadius.all(Radius.circular(32)),
+              child: Image(
+                image: AssetImage(
+                  'assets/images/me.jpg',
+                ),
+                width: 150,
+                height: 150,
+              ),
+            ),
+            SizedBox(
+              height: 24,
+            ),
+            Text(
+              "Zulqarnain Sajol",
+              style: TextStyle(fontSize: 34, fontWeight: FontWeight.bold),
+            ),
+            SizedBox(
+              height: 14,
+            ),
+            Text("Dhaka Bangladesh"),
+            SizedBox(
+              height: 24,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Column(
+                  children: [
+                    Text("567"),
+                    Text("FOllowars"),
+                  ],
+                ),
+                Column(
+                  children: [
+                    Text("567"),
+                    Text("post"),
+                  ],
+                ),
+                Column(
+                  children: [
+                    Text("567"),
+                    Text("following"),
+                  ],
+                ),
+              ],
+            ),
+            Divider(
+            height: 23,
+            indent: 3,
+            ),
+             SizedBox(
+             width: 455,
+                  height: 208,
+               child: Image(
+                  image: AssetImage(
+                    'assets/images/me.jpg',
+                  ),
+                  
+                  ),
+             ),
+          ],
+          
+        ),
+      ),
     );
   }
 }
