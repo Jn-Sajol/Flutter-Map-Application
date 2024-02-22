c14d417268e42adb03cedca2bb078207ad5975c5
Jn-Sajol <julkar10121@gmail.com>
2024-02-22 10:22:48 +0600

    adding navigator

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,13 +1,45 @@
+import 'package:birthday_app/pages/chat_page.dart';
+import 'package:birthday_app/pages/map_page.dart';
+import 'package:birthday_app/pages/profile_page.dart';
 import 'package:flutter/material.dart';
 
-class HomePage extends StatelessWidget{
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
   @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  final List<Widget> items = [ProfilePage(), ChatPage(), MapPage()];
+  var currentindex = 0;
+  @override
   Widget build(BuildContext context) {
-    return const  Scaffold(
-    body: Text('hello body'),
+    return Scaffold(
+      body: items[currentindex],
+      bottomNavigationBar: BottomNavigationBar(
+        backgroundColor: const Color.fromARGB(255, 3, 18, 48),
+        selectedItemColor: Colors.amber,
+        showSelectedLabels: false,
+        showUnselectedLabels: false,
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: '',
+          ),
+          BottomNavigationBarItem(icon: Icon(Icons.phone), label: ''),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.email),
+            label: '',
+          )
+        ],
+        currentIndex: currentindex,
+        onTap: (index) {
+          setState(() {
+            currentindex = index;
+          });
+        },
+      ),
     );
   }
-
 }
