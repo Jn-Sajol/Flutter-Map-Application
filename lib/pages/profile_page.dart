2b3461ed5bb3995beafc65c1cd4027069485672f
Jn-Sajol <julkar10121@gmail.com>
2024-02-22 15:21:01 +0600

    adding popupbutton

--- lib/pages/profile_page.dart
+++ lib/pages/profile_page.dart
@@ -1,13 +1,25 @@
 import 'package:flutter/material.dart';
 
 class ProfilePage extends StatelessWidget {
-  const ProfilePage({super.key});
-
+   ProfilePage({super.key});
+final  List<String> menu = ['add','edit'];
   @override
   Widget build(BuildContext context) {
     return  Scaffold(
     appBar: AppBar(
     title: const Text('Profile page'),
+    actions: [
+    PopupMenuButton(
+    itemBuilder:(context) => [
+    const PopupMenuItem(
+    child: Text('Edit')),
+    const PopupMenuItem(
+    child: Text('add')),
+    const PopupMenuItem(
+    child: Text('setting'))
+    ]
+    ),
+    ],
     ),
     );
   }
