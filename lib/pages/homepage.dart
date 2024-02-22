e3d399963a2f11cbff7fea3599b7e9dd5273596f
Jn-Sajol <julkar10121@gmail.com>
2024-02-22 08:59:16 +0600

    styling homepage

--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -5,39 +5,70 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return   Scaffold(
-        body: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Column(
-                children: [
-                const Text('Hello!, Welcome Back'),
-                const Text('Login To Continue'),
-                TextField(
-                decoration: const InputDecoration(
-                hintText:'add number',
-                border: OutlineInputBorder(
-                borderRadius: BorderRadius.all(Radius.circular(34))
-                )
-                ),
-                onTap: (){
-                
-                },
-                ),
-                const SizedBox(height: 23,),
-                TextField(
-                decoration: const InputDecoration(
-                hintText:'add number',
-                border: OutlineInputBorder(
-                borderRadius: BorderRadius.all(Radius.circular(34))
-                )
-                ),
-                onTap: (){
-                
-                },
-                )
-                ],
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.all(12.0),
+        child: Column(
+          children: [
+            const SizedBox(
+              height: 30,
+            ),
+            const Text(
+              'Hello!, Welcome Back',
+              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            const Text('Login To Continue'),
+            const SizedBox(
+              height: 30,
+            ),
+            TextField(
+              decoration: const InputDecoration(
+                  hintText: 'Enter email',
+                  filled: true,
+                  fillColor: Colors.white10,
+                  border: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(14)))),
+              onTap: () {},
+            ),
+            const SizedBox(
+              height: 23,
+            ),
+            TextField(
+              decoration: const InputDecoration(
+                  hintText: 'Enter Password',
+                  filled: true,
+                  fillColor: Colors.white10,
+                  border: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(14)))),
+              onTap: () {},
+            ),
+            Align(
+              alignment: Alignment.topRight,
+              child: TextButton(
+                  onPressed: () {}, child: const Text('Forgot Password?')),
+            ),
+            SizedBox(height: 30,),
+            SizedBox(
+              // width: 45,
+              child: ElevatedButton(
+                onPressed: () {},
+                style: const ButtonStyle(
+                    backgroundColor:
+                        MaterialStatePropertyAll(Colors.amberAccent),
+                    foregroundColor:
+                        MaterialStatePropertyAll(Colors.white70),
+                        minimumSize: MaterialStatePropertyAll(Size(267, 48))
+                        ),
+                        
+                child: const Text("Log In", style: TextStyle(color: Colors.black),),
               ),
+            ),
+          ],
         ),
+      ),
     );
   }
 }
