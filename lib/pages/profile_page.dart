52ff82244ae80be094fe67db2d5a0d0f81eae4be
Jn-Sajol <julkar10121@gmail.com>
2024-02-23 23:38:33 +0600

    profile update

--- lib/pages/profile_page.dart
+++ lib/pages/profile_page.dart
@@ -19,15 +19,15 @@ class ProfilePage extends StatelessWidget {
                   ]),
         ],
       ),
-      body: const Padding(
-        padding: EdgeInsets.all(12.0),
+      body: const  Padding(
+        padding: EdgeInsets.all(18.0),
         child: Column(
           children: [
             SizedBox(
               height: 14,
             ),
             ClipRRect(
-            borderRadius: BorderRadius.all(Radius.circular(32)),
+              borderRadius: BorderRadius.all(Radius.circular(32)),
               child: Image(
                 image: AssetImage(
                   'assets/images/me.jpg',
@@ -44,7 +44,7 @@ class ProfilePage extends StatelessWidget {
               style: TextStyle(fontSize: 34, fontWeight: FontWeight.bold),
             ),
             SizedBox(
-              height: 14,
+              height: 5,
             ),
             Text("Dhaka Bangladesh"),
             SizedBox(
@@ -74,21 +74,26 @@ class ProfilePage extends StatelessWidget {
               ],
             ),
             Divider(
-            height: 23,
-            indent: 3,
+              height: 23,
+              indent: 3,
             ),
-             SizedBox(
-             width: 455,
-                  height: 208,
-               child: Image(
-                  image: AssetImage(
-                    'assets/images/me.jpg',
-                  ),
-                  
-                  ),
-             ),
+            Expanded(
+              child: Image(
+                image: AssetImage(
+                  'assets/images/me.jpg',
+                ),
+              ),
+            ),
+            // Container(
+            // height: 230,
+            // width: 480,
+            // decoration: BoxDecoration(
+            // borderRadius: BorderRadius.all(Radius.circular(24)),
+            // color: Colors.amber,
+            // ),
+            // child: Image(image: AssetImage('assets/images/me.jpg')),
+            // )
           ],
-          
         ),
       ),
     );
